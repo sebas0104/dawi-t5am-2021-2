e1307329816f918c9c22d42c1517bcb600aa7589
SebastiánAranda <65477713+sebas0104@users.noreply.github.com>
2021-09-16 21:51:42 -0500

    se agrego el  login y funciona

--- src/main/java/app/FrmLogin.java
+++ src/main/java/app/FrmLogin.java
@@ -14,6 +14,7 @@ import modelo.Usuario;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -89,6 +90,41 @@ public class FrmLogin extends JFrame {
 	
 	
 	void registrar() {
+		//obtener la conexion con mi base de dato -->DAOFactori fabrica
+				EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+				
+				//creo los DAO usando la fabrica  
+			EntityManager em = fabrica.createEntityManager();
+			
+			//-- proceso: validar  un usuario segun su usuario y clave--> procedimiento almacenados
+			
+			String sql2 = "select u from Usuario u where u.usuario = :xusr and u.clave = :xcla";//JPA
+			
+
+			TypedQuery<Usuario> query = em.createQuery(sql2, Usuario.class);
+					query.setParameter("xusr", "U002@gmail.com");
+					query.setParameter("xcla", "10002");
+					
+			
+			
+			Usuario u =null;
+			try {
+				u = query.getSingleResult();
+			} catch (Exception e) {
+				
+			}
+			
+			if (u==null) {
+				System.out.println("codigo no existe");
+			}else {
+				System.out.println("Bienvenido:"+u.getNombre());
+				System.out.println(u);
+			}
+			
+			
+			em.close();
+			
+
 		
 	}
 }
